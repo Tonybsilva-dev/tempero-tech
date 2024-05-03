f809da0c4d91eed71e99552643c64736829f0207
Antonio Silva <contato@antoniobsilva.com>
2024-05-02 23:41:12 -0300

    update product by category page

--- app/categories/[id]/products/page.tsx
+++ app/categories/[id]/products/page.tsx
@@ -32,18 +32,25 @@ const CategoriesPage = async ({ params: { id } }: CategoriesPageProps) => {
 
   return (
     <>
-      <div className="px-5 py-6">
-        <h2 className="mb-6 text-lg font-semibold">{category.name}</h2>
-        <div className="grid grid-cols-2 justify-items-center gap-6 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-8">
-          {category.products.map((product) => (
-            <ProductItem
-              key={product.id}
-              product={product}
-              className="min-w-full"
-            />
-          ))}
+      <section
+        className="bg-transparent px-6 py-16 md:px-8 lg:px-10"
+        id="products"
+      >
+        <div className="container mx-auto">
+          <h2 className="mb-8 flex justify-between text-3xl font-bold">
+            {category.name}
+          </h2>
+          <div className="grid grid-cols-2 justify-items-center gap-6 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-8">
+            {category.products.map((product) => (
+              <ProductItem
+                key={product.id}
+                product={product}
+                className="min-w-full"
+              />
+            ))}
+          </div>
         </div>
-      </div>
+      </section>
     </>
   );
 };
