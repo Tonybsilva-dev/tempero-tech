1559b288e05ea79230a87f2d0c11ba46a87b1c5c
Antonio Silva <contato@antoniobsilva.com>
2024-05-06 22:18:12 -0300

    feat: add github provider

--- app/_lib/auth.ts
+++ app/_lib/auth.ts
@@ -1,5 +1,7 @@
 import { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
+
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from "@/app/_lib/prisma";
 import { Adapter } from "next-auth/adapters";
@@ -12,6 +14,10 @@ export const authOptions: AuthOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    })
   ],
   callbacks: {
     async session({ session, user }: any) {
